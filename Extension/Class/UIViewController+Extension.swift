0182c5ca6fba56a9fe750484b1eee30ac16dbbad
Ryan <214522523@qq.com>
2018-12-20 10:22:44 +0800

    fix

--- Extension/Class/UIViewController+Extension.swift
+++ Extension/Class/UIViewController+Extension.swift
@@ -8,18 +8,13 @@
 
 import UIKit
 
-
 extension UIViewController {
     
     private struct AssociatedKeys {
-        static var presentedKey = "UIViewController.presentedAnimatedTransitioning"
-        static var dismissedKey = "UIViewController.dismissedANimatedTransitioning"
-        static var presentationKey = "UIViewController.presentationController"
-        
         static var operaionKey = "UIViewController.operaion"
     }
     
-    private var transitionOperation: TransitionOperation? {
+    var transitionOperation: TransitionOperation? {
         get {
             return objc_getAssociatedObject(self,
                                             &AssociatedKeys.operaionKey) as? TransitionOperation
@@ -29,94 +24,41 @@ extension UIViewController {
                                      &AssociatedKeys.operaionKey,
                                      newValue,
                                      .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        }
-    }
-    
-    var presentedAnimatedTransitioning: UIViewControllerAnimatedTransitioning? {
-        get {
-            return objc_getAssociatedObject(self,
-                                            &AssociatedKeys.presentedKey) as? UIViewControllerAnimatedTransitioning
-        }
-        set {
-            objc_setAssociatedObject(self,
-                                     &AssociatedKeys.presentedKey,
-                                     newValue,
-                                     .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
             
-            if transitionOperation == nil { transitionOperation = TransitionOperation() }
-            transitionOperation?.ownerVc = self
-            self.transitioningDelegate = transitionOperation
             self.modalPresentationStyle = .custom
-        }
-    }
-    
-    var dismissedAnimatedTransitioning: UIViewControllerAnimatedTransitioning? {
-        get {
-            return objc_getAssociatedObject(self,
-                                            &AssociatedKeys.dismissedKey) as? UIViewControllerAnimatedTransitioning
-        }
-        set {
-            objc_setAssociatedObject(self,
-                                     &AssociatedKeys.dismissedKey,
-                                     newValue,
-                                     .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-            
-            if transitionOperation == nil { transitionOperation = TransitionOperation() }
-            transitionOperation?.ownerVc = self
-            self.transitioningDelegate = transitionOperation
-            self.modalPresentationStyle = .custom
-        }
-    }
-    
-    var presentationController: UIPresentationController? {
-        get {
-            return objc_getAssociatedObject(self,
-                                            &AssociatedKeys.presentationKey) as? UIPresentationController
-        }
-        set {
-            objc_setAssociatedObject(self,
-                                     &AssociatedKeys.presentationKey,
-                                     newValue,
-                                     .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-            
-            if transitionOperation == nil { transitionOperation = TransitionOperation() }
-            transitionOperation?.ownerVc = self
-            self.transitioningDelegate = transitionOperation
-            self.modalPresentationStyle = .custom
+            self.transitioningDelegate = newValue
         }
     }
 }
 
 
-fileprivate class TransitionOperation: NSObject, UIViewControllerTransitioningDelegate {
+class TransitionOperation: NSObject, UIViewControllerTransitioningDelegate {
     
-    weak var ownerVc: UIViewController?
+    typealias FetchPresentationControllerHandle = ((_ presented: UIViewController, _ presenting: UIViewController?) -> UIPresentationController)
+    
+    private var presentedTransitioning: UIViewControllerAnimatedTransitioning?
+    private var dismissedTransitioning: UIViewControllerAnimatedTransitioning?
+    private var presentation: FetchPresentationControllerHandle?
+    
+    init(presentedTransitioning: UIViewControllerAnimatedTransitioning? = nil,
+         dismissedTransitioning: UIViewControllerAnimatedTransitioning? = nil,
+         presentationController: FetchPresentationControllerHandle? = nil) {
+        super.init()
+        self.presentedTransitioning = presentedTransitioning
+        self.dismissedTransitioning = dismissedTransitioning
+        self.presentation = presentationController
+    }
     
     func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        guard let vc = ownerVc,
-            let dismissedAnimatedTransitioning = vc.dismissedAnimatedTransitioning,
-            dismissed == vc else {
-                return nil
-        }
-        return dismissedAnimatedTransitioning
+        return dismissedTransitioning
     }
     
     func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        guard let vc = ownerVc,
-            let presentedAnimatedTransitioning = vc.presentedAnimatedTransitioning,
-            presented == vc else {
-                return nil
-        }
-        return presentedAnimatedTransitioning
+        return presentedTransitioning
     }
     
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
-        guard let vc = ownerVc,
-            let presentationController = vc.presentationController,
-            presented == vc else {
-                return nil
-        }
-        return presentationController
+        return presentation?(presented, presenting)
     }
 }
 
