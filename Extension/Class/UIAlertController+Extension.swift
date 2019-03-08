84f2a42e88e81c683d1811d90d95385de6da8b3e
Ryan <>
2019-03-08 14:50:41 +0800

    UIAlertController文案属性

--- Extension/Class/UIAlertController+Extension.swift
+++ Extension/Class/UIAlertController+Extension.swift
@@ -10,31 +10,55 @@ import UIKit
 
 extension UIAlertController {
     
-    /// 设置文本对其方式
-    func setMessageTextAlignment(textAlignment: NSTextAlignment) {
-        
-        func enumrateSubiewsInView(view: UIView) {
-            let subViews = view.subviews
-            if subViews.count == 0 {
-                return
-            }
-            
-            for i in 0..<subViews.count {
-                let subView = subViews[i]
-                enumrateSubiewsInView(view: subView)
-                if subView is UILabel {
-                    let label = subView as! UILabel
-                    if label.text == message {
-                        label.textAlignment = textAlignment
-                    }
-                }
-            }
+    var titleAttributes: [NSAttributedString.Key : Any]? {
+        get {
+            return value(forKey: "attributedTitle") as? [NSAttributedString.Key : Any]
         }
-        
-        enumrateSubiewsInView(view: view)
+        set {
+            guard let value = newValue else { return }
+            let attributes = NSAttributedString(string: title ?? "", attributes: value)
+            setValue(attributes, forKey: "attributedTitle")
+        }
     }
     
+    var messageAttributes: [NSAttributedString.Key : Any]? {
+        get {
+            return value(forKey: "attributedMessage") as? [NSAttributedString.Key : Any]
+        }
+        set {
+            guard let value = newValue else { return }
+            let attributes = NSAttributedString(string: message ?? "", attributes: value)
+            setValue(attributes, forKey: "attributedMessage")
+        }
+    }
     
+    open override func setValue(_ value: Any?, forUndefinedKey key: String) {
+        print("没找到" + key)
+    }
     
+    open override func value(forUndefinedKey key: String) -> Any? {
+        print("没找到" + key)
+        return nil
+    }
+}
+
+extension UIAlertAction {
     
+    var titleColor: UIColor? {
+        get {
+            return value(forKey: "titleTextColor") as? UIColor
+        }
+        set {
+            setValue(newValue, forKey: "titleTextColor")
+        }
+    }
+    
+    open override func setValue(_ value: Any?, forUndefinedKey key: String) {
+        print("没找到" + key)
+    }
+    
+    open override func value(forUndefinedKey key: String) -> Any? {
+        print("没找到" + key)
+        return nil
+    }
 }
--- Extension/ViewController.swift
+++ Extension/ViewController.swift
@@ -18,9 +18,9 @@ class ViewController: UIViewController {
 //        btn.structMode = .leftTextRightImage(interval: 0)
 //        btn.structMode = .leftImageRightText(interval: 0)
 //        btn.structMode = .topTextBottomImage(interval: 0)
-        btn.structMode = .topImageBottomText(interval: 0)
+//        btn.structMode = .topImageBottomText(interval: 0)
         
-        imageView.image = imageView.image?.rotation(by: CGFloat(Double.pi / 2))
+//        imageView.image = imageView.image?.rotation(by: CGFloat(Double.pi / 2))
 //        imageView.image = imageView.image?.horizontalFlip()
 //        imageView.image = imageView.image?.verticalFlip()
      
@@ -33,17 +33,42 @@ class ViewController: UIViewController {
 //
         
 //        let dd = "0733-82343232"
-        let cc = "89ju🔓😁"
-        if cc.contain(with: .emoji) {
-            print("isAlpha true")
-        }
+//        let cc = "89ju🔓😁"
+//        if cc.contain(with: .emoji) {
+//            print("isAlpha true")
+//        }
+//
+//        var date = Date()
+//        date.day = 10
+//        print(date.toString("yyyy-MM-dd HH:mm"))
+//        print(date.weekday)
         
-        var date = Date()
-        date.day = 10
-        print(date.toString("yyyy-MM-dd HH:mm"))
-        print(date.weekday)
     }
 
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let alert = UIAlertController(title: "退保证金后，将再也无法获得收益",
+                                      message: "从 3 月 26 日起，所有未交保证金的店主，不再享受店主权益（包括但不限于下级店主返佣、店主店铺销售差额收益、任务奖励）",
+                                      preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "不退了", style: .cancel, handler: nil)
+        let confirm = UIAlertAction(title: "退保证金", style: .default, handler: nil)
+        alert.addAction(confirm)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .left
+        paragraphStyle.lineSpacing = 3
+        alert.titleAttributes = [NSAttributedString.Key.foregroundColor : UIColor.red,
+                                 NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 15)]
+        alert.messageAttributes = [NSAttributedString.Key.foregroundColor : UIColor.lightGray,
+                                   NSAttributedString.Key.paragraphStyle : paragraphStyle]
+        cancel.titleColor = UIColor.red
+        confirm.titleColor = UIColor.lightGray
+        
+    }
+    
 }
 
