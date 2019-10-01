c8a5bd8bb41f30fe6d6117e036cc340ecb4ddb02
Ryan <214522523@qq.com>
2019-10-02 01:39:26 +0800

    update

---
+++ Extension/Class/Int+Extension.swift
@@ -0,0 +1,33 @@
+//
+//  Int+Extension.swift
+//  Extension
+//
+//  Created by 赖灵伟 on 2019/10/2.
+//  Copyright © 2019 Ryan. All rights reserved.
+//
+
+import UIKit
+
+extension Int {
+    
+    var toFloat: Float {
+        return Float(self)
+    }
+    
+    var toCGFloat: CGFloat {
+        return CGFloat(self)
+    }
+    
+    var toDouble: Double {
+        return Double(self)
+    }
+    
+    var toInt64: Int64 {
+        return Int64(self)
+    }
+    
+    var toUInt64: UInt64 {
+        return UInt64(self)
+    }
+    
+}
